a4f52ae976d2e8f0b0161d090b00c0400b335ee0
DoogieMac <xjoelee@gmail.com>
2025-01-14 17:59:38 +0900

    joeMac:250114-1759:

---
+++ doogie_backup/_skroute/skrouteadd_200406.sh
@@ -0,0 +1,55 @@
+# default skb innet-office
+sudo route add -net 10/8 10.240.74.1
+sudo route add -net 12/8 10.240.74.1
+
+# nate.sk.com
+sudo route add -net 10.178/16 	10.240.74.1
+
+# *.sk.com
+sudo route add -net 210.211.67/24 	10.240.74.1
+sudo route add -net 210.211.69/24 	10.240.74.1
+sudo route add -net 210.211.70/24 	10.240.74.1
+sudo route add -net 211.115.106/24 	10.240.74.1
+
+# mail.skbroadband.com
+sudo route add -net 58.123.195/24 	10.240.74.1
+
+# swing.skbroadband.com
+sudo route add -net 172.31.130/24 	10.240.74.1
+sudo route add -net 220.103.244/24 	10.240.74.1
+
+# ???
+sudo route add -net 13.107.5/24 	10.240.74.1
+sudo route add -net 20.190.141/24 	10.240.74.1
+sudo route add -net 23.61.244/24 	10.240.74.1
+sudo route add -net 40.90.189/24 	10.240.74.1
+sudo route add -net 61.111.58/24 	10.240.74.1
+sudo route add -net 104.74.207/24 	10.240.74.1
+sudo route add -net 117.18.237/24 	10.240.74.1
+sudo route add -net 125.132.2/24 	10.240.74.1
+sudo route add -net 168.63.153/24 	10.240.74.1
+sudo route add -net 192.229.232/24 	10.240.74.1
+sudo route add -net 209.167.231/24 	10.240.74.1
+
+# *.outlook
+sudo route add -net 218.234.31/24 	10.240.74.1
+
+# VRPMS, virobot
+sudo route add -net 218.232.107/24 	10.240.74.1
+
+# ???
+sudo route add -net 52.139/16 	10.240.74.1
+sudo route add -net 52.184/16 	10.240.74.1
+sudo route add -net 52.172/16 	10.240.74.1
+sudo route add -net 52.175/16 	10.240.74.1
+sudo route add -net 52.219/16 	10.240.74.1
+sudo route add -net 52.109/16 	10.240.74.1
+sudo route add -net 52.229/16 	10.240.74.1
+
+# maybe google, gmail
+#sudo route add -net 172.217.26/24 	10.240.74.1
+#sudo route add -net 172.217.175/24 	10.240.74.1
+#sudo route add -net 172.217.25/24 	10.240.74.1
+#sudo route add -net 172.217.27/24 	10.240.74.1
+#sudo route add -net 172.217.161/24 	10.240.74.1
+
---
+++ doogie_backup/_skroute/tsisdel.sh
@@ -0,0 +1,19 @@
+#
+# SKB Office Net: Static Route Add
+#
+SKBGW="10.16.60.1"
+
+#
+# get Proxy IP list:
+#  curl http://12.4.11.218/skb.pac
+#
+
+while read line; do 
+  check=${line:0:1}
+  # echo $check
+
+  if [[ $check != "#" ]] && [[ -n $check ]]; then
+    #echo $line
+    sudo route delete -net $line $SKBGW
+  fi
+done < skroutelist
